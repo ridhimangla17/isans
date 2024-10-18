23f7622daedf1fbb8b9d0e7a4d61cf69d5db9816
ridhimangla17 <ridhimangla17@gmail.com>
2024-10-18 18:29:42 -0300

    added employee

--- rbc/src/main/java/isans/hackathon/rbc/entity/Employee.java
+++ rbc/src/main/java/isans/hackathon/rbc/entity/Employee.java
@@ -1,4 +1,41 @@
 package isans.hackathon.rbc.entity;
 
 public class Employee {
+    private Long employeeId;
+
+    private String firstName;
+    private String lastName;
+    private String authorization;
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAuthorization() {
+        return authorization;
+    }
+
+    public void setAuthorization(String authorization) {
+        this.authorization = authorization;
+    }
 }
--- rbc/src/main/java/isans/hackathon/rbc/service/EmployeeService.java
+++ rbc/src/main/java/isans/hackathon/rbc/service/EmployeeService.java
@@ -1,4 +1,45 @@
 package isans.hackathon.rbc.service;
+import com.google.cloud.firestore.*;
+import isans.hackathon.rbc.entity.Employee;
+import com.google.api.core.ApiFuture;
+import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ExecutionException;
+
+@Service
 public class EmployeeService {
+    private static final String COLLECTION_NAME = "employees";
+
+    public String saveEmployee(Employee employee) throws ExecutionException, InterruptedException {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        String documentId = String.valueOf(employee.getEmployeeId());
+        ApiFuture<WriteResult> collectionApiFuture = dbFirestore.collection(COLLECTION_NAME).document(documentId).set(employee);
+        return collectionApiFuture.get().getUpdateTime().toString();
+    }
+    public String updateAuthorization(Long employeeId, String newAuthorization) throws ExecutionException, InterruptedException {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+
+        String documentId = String.valueOf(employeeId);
+
+        ApiFuture<DocumentSnapshot> query = dbFirestore.collection(COLLECTION_NAME).document(documentId).get();
+        DocumentSnapshot document = query.get();
+
+        if (document.exists()) {
+            // Fetch existing employee object
+            Employee employee = document.toObject(Employee.class);
+
+            // Ensure employee object is not null
+            assert employee != null;
+
+            // Update the authorization field
+            employee.setAuthorization(newAuthorization);
+
+            // Save the updated employee object back to Firestore
+            ApiFuture<WriteResult> collectionApiFuture = dbFirestore.collection(COLLECTION_NAME).document(documentId).set(employee);
+            return collectionApiFuture.get().getUpdateTime().toString();
+        } else {
+            throw new RuntimeException("Employee with ID " + employeeId + " not found.");
+        }
+    }
 }
