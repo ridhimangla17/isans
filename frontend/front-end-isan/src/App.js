78c4e32703c4e025ce8ecabfe66d80cb5f42726b
bvarsani <br667430@dal.ca>
2024-10-19 02:47:02 -0300

    added client dashboard and login logout button

--- frontend/front-end-isan/src/App.js
+++ frontend/front-end-isan/src/App.js
@@ -59,23 +59,52 @@
 // export default App;
 
 
+// import React from 'react';
+// import Header from './Header';
+// import HeroSection from './HeroSection';
+// import InfoSection from './InfoSection';
+// import Footer from './Footer';
+// import './App.css';
+// import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <HeroSection />
+//       <InfoSection />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Header';
 import HeroSection from './HeroSection';
 import InfoSection from './InfoSection';
 import Footer from './Footer';
+import Dashboard from './ClientDashboard'; // Import the ClientDashboard component
 import './App.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <HeroSection />
-      <InfoSection />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          {/* Define your routes here */}
+          <Route path="/" element={<><HeroSection /><InfoSection /><Footer /></>} />
+          <Route path="/client-dashboard" element={<Dashboard />} />
+          {/* You can add more routes for different pages */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
