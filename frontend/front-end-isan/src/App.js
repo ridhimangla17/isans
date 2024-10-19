0d1ae95fd4f7cc132aab2ceeff88ecb446082520
ridhimangla17 <ridhimangla17@gmail.com>
2024-10-19 02:24:45 -0300

    connected application form

--- frontend/front-end-isan/src/App.js
+++ frontend/front-end-isan/src/App.js
@@ -6,6 +6,7 @@ import Footer from './Footer';
 import './App.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import PendingRequests from './EmployeeView/PendingRequests';
+import RegistrationForm from './EmployeeView/registrationform';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
@@ -15,6 +16,7 @@ function App() {
       <div className="App">
         <Header />
         <Routes>
+          <Route path="/register" element={<RegistrationForm />} />
           <Route path="/pending-requests" element={<PendingRequests />} />
         </Routes>
         <HeroSection />
