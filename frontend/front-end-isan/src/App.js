bc9e394f3ed4b9cc5675ddd8920fdeb260ce8858
bvarsani <br667430@dal.ca>
2024-10-19 05:11:07 -0300

    added color to program card and fixrd search bar

--- frontend/front-end-isan/src/App.js
+++ frontend/front-end-isan/src/App.js
@@ -8,7 +8,6 @@ import Dashboard from './ClientDashboard'; // Import the ClientDashboard compone
 import './App.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import PendingRequests from './EmployeeView/PendingRequests';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
@@ -16,8 +15,6 @@ function App() {
       <div className="App">
         <Header />
         <Routes>
-          {/* Define your routes here */}
-          <Route path="/" element={<><HeroSection /><InfoSection /><Footer /></>} />
           <Route path="/client-dashboard" element={<Dashboard />} />
           {/* You can add more routes for different pages */}
           <Route path="/pending-requests" element={<PendingRequests />} />
